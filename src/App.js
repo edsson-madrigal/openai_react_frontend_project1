118a4c1b55781358a6ce0f9c6322cf45a560d8cb
Edsson Madrigal <akairu@hotmail.com>
2023-09-17 18:22:37 -0600

    streams

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './screens/Home'
-import "./styles/bootstrap-custom.scss"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Stream from "./screens/Stream";
 
+import "./styles/bootstrap-custom.scss";
 
-function App(){
-    return (
-        <>  
-            <Router>
-                <Routes>
-                    <Route path='/' element={
-                        <Home/>
-                    }>
-                    </Route>
-                </Routes>
-            </Router>
-        </>
-    )
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/stream" element={<Stream />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
